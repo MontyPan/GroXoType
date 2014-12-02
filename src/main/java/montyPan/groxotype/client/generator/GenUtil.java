c53e950819cfcae8a4240e1ff8ff2159afdaf64f
MontyPan <monty.pan@hwacom.com>
2014-12-02 21:14:41 +0800

    setAttribute() 改為單純 setter，實際 gen 字串的功能挪到 genAttribute()

--- src/main/java/montyPan/groxotype/client/generator/GenUtil.java
+++ src/main/java/montyPan/groxotype/client/generator/GenUtil.java
@@ -8,20 +8,37 @@ import com.sencha.gxt.widget.core.client.Component;
 public class GenUtil {
 	public static final String ATTRIBUTE = "ATTRIBUTE";
 	public static final String CONTEXT = "CONTEXT";
+	public static final String CHILD = "--CHILD--";
+	public static final String NAMESPACE = "--NS--";
 
 	public static void setAttribute(Component component, HashMap<String, Object> map) {
 		if (map == null || map.size() == 0) { return; }
 
+		component.setData(ATTRIBUTE, map);
+	}
+	
+	public static String genAttribute(Component component) {
+		HashMap<String, Object> map = component.getData(ATTRIBUTE);
+		if (map == null || map.size() == 0) { return ""; }
+		
 		StringBuffer result = new StringBuffer();
 		for (String key : map.keySet()) {
 			result.append(" " + key + "=\"" + map.get(key) + "\"");
 		}
-		component.setData(ATTRIBUTE, result.toString());
+		return result.toString();
 	}
 
 	public static void setContext(Component component, ArrayList<String> context) {
 		if (context == null || context.size() == 0) { return; }
 
 		component.setData(CONTEXT, context);
+	}
+	
+	public static String genTab(int level) {
+		StringBuffer result = new StringBuffer();
+		for (int i = 0; i < level; i++) {
+			result.append('\t');
+		}
+		return result.toString();
 	}
 }
